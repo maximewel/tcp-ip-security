0381af2935847ac7e5433639ab0f42a94eec4b35
Lucas Fridez <lucas@fridez.dev>
2021-04-22 12:10:34 +0200

    Add: isn in test.py

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 from scapy.all import *
 from scapy.layers.inet import *
 
-[print(f"{p.summary()} with ttl {p.ttl}") for p in IP(ttl=(1,5))/ICMP()]
+# [print(f"{p.summary()} with ttl {p.ttl}") for p in IP(ttl=(1,5))/ICMP()]
+
+
+ans, unans = srloop(IP(dst="betweenfriends.forumactif.com")/TCP(dport=80,flags="S"))
+
+temp = 0
+for s, r in ans:
+   temp = r[TCP].seq - temp
+   print("%d\t+%d" % (r[TCP].seq, temp))
