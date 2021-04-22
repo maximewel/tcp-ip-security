db5a71ab13505a4aa9e7b1f39d925f6b77848ae5
Lucas Fridez <lucas@fridez.dev>
2021-04-22 14:11:05 +0200

    Docs: Comment all files

--- send-packet/packet.py
+++ send-packet/packet.py
@@ -10,23 +10,35 @@ import argparse
 
 
 def main(dst, port):
+    """Send packets with regular interval
+
+    Args:
+        dst (str): Destination address
+        port (int): Destination port
+    """
     DST = dst
     D_PORT = port
     SLEEP_S = 1
     NBR_MSG = 100
 
+    # Send packets
     for i in range(1, NBR_MSG + 1):
         packet = IP(dst=DST)/TCP(dport=D_PORT)/f"Hi, it is message number {i}"
         sleep(SLEEP_S)
         send(packet)
         print(f"--> Message number {i} sent !")
 
-
-if __name__ == "__main__":
+def __configure_argparse__():
     parser = argparse.ArgumentParser(description='Foo')
     requiredNamed = parser.add_argument_group('required named arguments')
     requiredNamed.add_argument('-d', '--dst', help='Destination address', required=True)
     requiredNamed.add_argument('-p', '--port', help='Destination port', required=True, type=int)
+
+    return parser
+
+# Entry point
+if __name__ == "__main__":
+    parser = __configure_argparse__() 
     args = parser.parse_args()
 
     main(args.dst, args.port)
